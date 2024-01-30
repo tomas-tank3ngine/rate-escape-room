53075487a2c005a94b293c2880e52d1e04bd5001
tomas-tank3ngine <tomasmartinez1424@gmail.com>
2024-01-30 06:39:22 -0500

    header visuals and styling

--- src/assets/components/Header/Header.js
+++ src/assets/components/Header/Header.js
@@ -1,10 +1,41 @@
+import Headshot from '../Headshot/Headshot';
 import './Header.scss'
+import { Link } from 'react-router-dom';
+import menuIcon from '../../icons/menu_fill.svg'
 
 function Header(){
+
+    const menuHandler = (()=>{
+        console.log("menu button clicked");
+    })
+
     return(
-        <>
-        
-        </>
+        <section className="header">
+            <nav className="nav-bar">
+                <button className="menu" onClick={menuHandler}>
+                    <img src={menuIcon} alt="menuIcon" className="menu__icon" />
+                </button>
+                <ul className="pages">
+                    <li className="pages__link">
+                        <Link to="/" className="pages__link--text">Homepage</Link>
+                    </li>
+                    <li className="pages__link">
+                        <Link to="/rooms" className="pages__link--text">Rooms</Link>
+                    </li>
+                    <li className="pages__link">
+                        <Link to="/" className="pages__link--text">My Favourites</Link>
+                    </li>
+                </ul>
+
+            </nav>
+            <Link to="/"className="logo">
+                <img src="" alt="Rate Escape Rooms" className="logo__image" />
+            </Link>
+            <section className="login">
+                <Link to="/login" className='login__link'>sign-in</Link>
+                <Headshot />
+            </section>
+        </section>
     )
 }
 
