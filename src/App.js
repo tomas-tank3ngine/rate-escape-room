2baada88170c39fa17bf376f8db3e3b4ac8e21d3
tomas-tank3ngine <tomasmartinez1424@gmail.com>
2024-01-30 05:24:43 -0500

    add room details route

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Header from "./assets/components/Header/Header.js"
 import Footer from "./assets/components/Footer/Footer.js"
 import Homepage from "./assets/pages/HomePage/Homepage.js"
 import NotFoundPage from "./assets/pages/NotFoundPage/NotFoundPage.js"
+import RoomsPage from "./assets/pages/RoomsPage/RoomsPage.js";
+import RoomDetailsPage from "./assets/pages/RoomDetailsPage/RoomDetailsPage.js";
 
 
 function App() {
@@ -14,12 +16,12 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Homepage />} />
-          {/* <Route path="/rooms" element={<Homepage />} />
-          <Route path="/rooms:roomId" element={<Homepage />} />
-          <Route path="/rooms:roomId/rate" element={<Homepage />} />
-          <Route path="/login" element={<Homepage />} />
-          <Route path="/create-account" element={<Homepage />} />
-          <Route path="/rooms-nearby" element={<Homepage />} /> */}
+          <Route path="/rooms" element={<RoomsPage />} />
+          <Route path="/rooms:roomId" element={<RoomDetailsPage />} />
+          {/* <Route path="/rooms:roomId/rate" element={<Homepage />} /> */}
+          {/* <Route path="/login" element={<Homepage />} /> */}
+          {/* <Route path="/create-account" element={<Homepage />} /> */}
+          {/* <Route path="/rooms-nearby" element={<Homepage />} /> */}
 
 
           <Route path="*" element={<NotFoundPage />} />
