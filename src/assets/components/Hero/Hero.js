1a83c5d5fb2ac56b7be24f99631d88bf00545179
tomas-tank3ngine <tomasmartinez1424@gmail.com>
2024-01-30 22:54:53 -0500

    add hero mobile styling

--- src/assets/components/Hero/Hero.js
+++ src/assets/components/Hero/Hero.js
@@ -1,9 +1,20 @@
 import './Hero.scss';
+import HeroImage from '../../images/b&wherokeys2.png'
+import { Link } from 'react-router-dom';
 
 function Hero(){
     return(
         <section className="hero">
-            Hero
+            <section className="hero__container">
+                <Link to="/rooms" className="cta">
+                    <p className="cta__p">Room Rankings</p>
+                </Link>
+            </section>
+            <section className="hero__container">
+            <Link to="/rooms-nearby" className="cta">
+                <p className="cta__p">Rooms Near Me</p>
+            </Link>
+            </section>
         </section>
     )
 }
