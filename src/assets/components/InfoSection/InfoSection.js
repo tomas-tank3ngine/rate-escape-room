99eca60644d57320d5b2273e52fed0614e2f1d9c
tomas-tank3ngine <tomasmartinez1424@gmail.com>
2024-01-31 02:47:20 -0500

    add info section styling

--- src/assets/components/InfoSection/InfoSection.js
+++ src/assets/components/InfoSection/InfoSection.js
@@ -3,8 +3,30 @@ import './InfoSection.scss'
 function InfoSection(){
     return(
         <>
-            <section className="info-section">
-                <h2 className="info-section__header">WHY PLAY ESCAPE ROOMS?</h2>
+            <section className="info">
+                <h2 className="info__header">WHY PLAY ESCAPE ROOMS?</h2>
+                <section className="info__child-container">
+                    <div className="why-play">
+                        <img src="" alt="" className="why-play__image" />
+                        <h3 className="why-play__header">Reason</h3>
+                        <p className="why-play__p">text</p>
+                    </div>
+                    <div className="why-play">
+                        <img src="" alt="" className="why-play__image" />
+                        <h3 className="why-play__header">Reason</h3>
+                        <p className="why-play__p">text</p>
+                    </div>
+                    <div className="why-play">
+                        <img src="" alt="" className="why-play__image" />
+                        <h3 className="why-play__header">Reason</h3>
+                        <p className="why-play__p">text</p>
+                    </div>
+                    <div className="why-play">
+                        <img src="" alt="" className="why-play__image" />
+                        <h3 className="why-play__header">Reason</h3>
+                        <p className="why-play__p">text</p>
+                    </div>
+                </section>
             </section>
         </>
     )
