eedb0e0409d44c73095b09dd1a47cdd1fa7a21c4
tomas-tank3ngine <tomasmartinez1424@gmail.com>
2024-02-04 16:07:29 -0500

    minor syntax changes in Room Overview

--- src/assets/components/RoomOverview/RoomOverview.js
+++ src/assets/components/RoomOverview/RoomOverview.js
@@ -1,33 +1,38 @@
-import { Link } from 'react-router-dom';
-import './RoomOverview.scss'
-import StarRating from '../StarRating/StarRating';
-import FavRoomButton from '../FavRoomButton/FavRoomButton';
+import { Link } from "react-router-dom";
+import "./RoomOverview.scss";
+import StarRating from "../StarRating/StarRating";
+import FavRoomButton from "../FavRoomButton/FavRoomButton";
+import { useEffect } from "react";
 
-function RoomOverview(){
+function RoomOverview() {
     // Dont forget to link the room id properly in the return()
-    return(
-        <>
-            <section className="room-overview">
-                <Link to="/rooms/:1"className="details-link">
-                    <p className="details-link__text">Room Details</p>
-                    <img src="" alt="thumbnail" className="details-link__thumbnail" />
-                </Link>
-                <section className="selected-info">
-                    <section className="selected-info__top-section">
-                        
-                        <FavRoomButton />
-                        <h2 className="selected-info__name">ROOM NAME</h2>
-                        <StarRating />
-                    </section>
-                    <p className="selected-info__description-text">DESCRIPTION</p>
-                    <section className="selected-info__bottom-section">
-                        <Link className="selected-info__website-link">WEBSITE URL</Link>
-                        <Link className="selected-info__share-link">SHARE URL</Link>
-                    </section>
+
+    <>
+        <section className="room-overview">
+            <Link to="/rooms/:1" className="details-link">
+                <p className="details-link__text">Room Details</p>
+                <img
+                    src=""
+                    alt="thumbnail"
+                    className="details-link__thumbnail"
+                />
+            </Link>
+            <section className="selected-info">
+                <section className="selected-info__top-section">
+                    <FavRoomButton />
+                    <h2 className="selected-info__name">ROOM NAME</h2>
+                    <StarRating />
+                </section>
+                <p className="selected-info__description-text">DESCRIPTION</p>
+                <section className="selected-info__bottom-section">
+                    <Link className="selected-info__website-link">
+                        WEBSITE URL
+                    </Link>
+                    <Link className="selected-info__share-link">SHARE URL</Link>
                 </section>
             </section>
-        </>
-    );
+        </section>
+    </>;
 }
 
 export default RoomOverview;
