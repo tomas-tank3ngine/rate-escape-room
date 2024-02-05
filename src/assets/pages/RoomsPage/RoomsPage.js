d90ff5611096751890e1459e212fbe29e045ff4a
tomas-tank3ngine <tomasmartinez1424@gmail.com>
2024-02-04 20:31:23 -0500

    setup some styling

--- src/assets/pages/RoomsPage/RoomsPage.js
+++ src/assets/pages/RoomsPage/RoomsPage.js
@@ -1,55 +1,52 @@
-import './RoomsPage.scss'
+import './RoomsPage.scss';
 import RoomsTable from '../../components/RoomsTable/RoomsTable';
 import RoomOverview from '../../components/RoomOverview/RoomOverview';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { allRoomsEndpoint } from '../../utils/api-utils';
-import { random } from 'node-forge';
 
-function RoomsPage({ responsive, user, allRooms }) {
+function RoomsPage({ responsive, user }) {
     const [allRooms, setAllRooms] = useState([]);
     const [selectedRoom, setSelectedRoom] = useState({});
     const [loading, setLoading] = useState(true);
-  
+
     useEffect(() => {
-      const fetchData = async () => {
-        try {
-          const response = await axios.get(allRoomsEndpoint());
-          console.log(response.data);
-          setAllRooms(response.data);
-  
-          const randomIndex = Math.floor(Math.random() * response.data.length);
-          const randomRoom = response.data[randomIndex];
-          console.log(randomRoom);
-          setSelectedRoom(randomRoom);
-        } catch (error) {
-          console.error('Error fetching data: ', error);
-        } finally {
-          setLoading(false);
-        }
-      };
-  
-      fetchData();
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(allRoomsEndpoint());
+                setAllRooms(response.data);
+
+                const randomIndex = Math.floor(Math.random() * response.data.length);
+                const randomRoom = response.data[randomIndex];
+                setSelectedRoom(randomRoom);
+            } catch (error) {
+                console.error('Error fetching data: ', error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchData();
     }, []);
-  
+
     return (
-      <main className="rooms-page">
-        {loading ? (
-          <p>Loading...</p>
-        ) : (
-          <>
-            <RoomOverview room={selectedRoom} />
-            <hr className="rooms-page__line-break"></hr>
-            <RoomsTable
-              responsive={responsive}
-              allRooms={allRooms}
-              selectedRoom={selectedRoom}
-              setSelectedRoom={setSelectedRoom}
-            />
-          </>
-        )}
-      </main>
+        <main className="rooms-page">
+            {loading ? (
+                <p>Loading...</p>
+            ) : (
+                <>
+                    <RoomOverview room={selectedRoom} />
+                    <hr className="rooms-page__line-break"></hr>
+                    <RoomsTable
+                        responsive={responsive}
+                        allRooms={allRooms}
+                        selectedRoom={selectedRoom}
+                        setSelectedRoom={setSelectedRoom}
+                    />
+                </>
+            )}
+        </main>
     );
-  }
+}
 
 export default RoomsPage;
