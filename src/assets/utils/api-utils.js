480108c09e4c96f1cc370b0e7767bd36fed7b066
tomas-tank3ngine <tomasmartinez1424@gmail.com>
2024-02-26 16:32:15 -0500

    fixed api url slash

--- src/assets/utils/api-utils.js
+++ src/assets/utils/api-utils.js
@@ -1,23 +1,23 @@
 export const API_URL = process.env.REACT_APP_API_URL;
 
-export const allRoomsEndpoint = () => API_URL + "rooms";
+export const allRoomsEndpoint = () => API_URL + "/rooms";
 
-export const singleRoomEndpoint = (roomId) => API_URL + "rooms/" + roomId;
+export const singleRoomEndpoint = (roomId) => API_URL + "/rooms/" + roomId;
 
-export const allReviewsOfRoomEndpoint = (roomId) => API_URL + "rooms/" + roomId + "/reviews";
+export const allReviewsOfRoomEndpoint = (roomId) => API_URL + "/rooms/" + roomId + "/reviews";
 
-export const allUsersEndpoint = () => API_URL + "users";
+export const allUsersEndpoint = () => API_URL + "/users";
 
-export const singleUserEndpoint = (userId) => API_URL + "users/" + userId;
+export const singleUserEndpoint = (userId) => API_URL + "/users/" + userId;
 
-export const singleUserFavoriteRoomsEndpoint = (userId) => API_URL + "users/" + userId + "/favorites";
+export const singleUserFavoriteRoomsEndpoint = (userId) => API_URL + "/users/" + userId + "/favorites";
 
-export const userFavoritesEndpoint = () => API_URL + "favorites";
+export const userFavoritesEndpoint = () => API_URL + "/favorites";
 
-export const currentUserEndpoint = () => API_URL + "users/account/current"
+export const currentUserEndpoint = () => API_URL + "/users/account/current"
 
-export const registerUserEndpoint = () => API_URL + "users/account/register"
+export const registerUserEndpoint = () => API_URL + "/users/account/register"
 
-export const loginUserEndpoint = () => API_URL + "users/account/login"
+export const loginUserEndpoint = () => API_URL + "/users/account/login"
 
 
